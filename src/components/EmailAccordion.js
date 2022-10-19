0d5877513f4f6a9befe91932f79cb5cd5ee9482b
capooods <carlo.d.capio@gmail.com>
2022-10-19 23:25:54 +0800

    Remove old accordion design

--- src/components/EmailAccordion.js
+++ src/components/EmailAccordion.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BsGrid3X2Gap, BsCircleFill } from 'react-icons/bs'
+
 import Accordion from './Accordion.js'
 
 function EmailAccordion() {
@@ -10,18 +10,6 @@ function EmailAccordion() {
         <Accordion
           title="Item 1"
           text="Test item" />
-            {/* <!-- Accordion end --></li> */}
-        {/* <label className="grid grid-cols-2">
-          <div className="flex flex-row items-center">
-            <BsGrid3X2Gap className="-rotate-90" />
-            <input type="checkbox" />
-            <BsCircleFill className="text-green-600" />
-            <div className="flex-col">
-              <div className="">12</div>
-              <div>JAN</div>
-            </div>
-          </div>
-        </label> */}
       </div>
     </div>
 )};
