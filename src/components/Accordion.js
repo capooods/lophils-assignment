6f9e8b2d01f1265bd1d8c5ea1b93a87f240d9dcc
capooods <carlo.d.capio@gmail.com>
2022-10-20 16:22:07 +0800

    Fix date indicator and sort it by descending order

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -10,12 +10,31 @@ function Accordion(props) {
   const [uniqueId] = useState(_uniqueId('email-'))
   console.log("accordion: " + JSON.stringify(props));
 
+  const time = new Date(props.time_sent);
+
   const toggleHandler = (e) => {
     setOpen(!open);
   }
 
- 
-
+  const monthConverter = (date) => {
+    const month = date;
+    const converted = ({
+      0: 'JAN',
+      1: 'FEB',
+      2: 'MAR',
+      3: 'APR',
+      4: 'MAY',
+      5: 'JUN',
+      6: 'JUL',
+      7: 'AUG',
+      8: 'SEP',
+      9: 'OCT',
+      10: 'NOV',
+      11: 'DEC',
+    })[month] ?? 'Default';
+    return converted;
+  };
+  
   const openAnimation = useSpring({
     to: { "maxHeight": open ? "100" : "0px" },
     config: { duration: "300" }
@@ -24,13 +43,13 @@ function Accordion(props) {
   const arrowOpenAnimation = useSpring({
     to: { "transform": open ? "rotate(90deg)" : "rotate(0deg)"},
     config: { duration: "250" }
-  })
+  });
 
   const buttonHover = useSpring({
     to: { "height": checkboxHover ? "40px" : "0px",
           "width": checkboxHover ? "40px" : "0px" },
     config: { duration: "100" }
-  })
+  });
   
   
   return(
@@ -52,9 +71,9 @@ function Accordion(props) {
           </div>
           
           {/* Date Div */}
-          <div className="flex-row text-center bg-gray-50 rounded-md border-2 border-gray-100 p-1 h-14 w-14">
-            <p className="text-lg">12</p>
-            <p className="text-sm">JAN</p>
+          <div className="flex-row flex-none text-center bg-gray-50 rounded-md border-2 border-gray-100 p-1 h-14 w-14">
+            <p className="text-lg">{time.getDate()}</p>
+            <p className="text-sm">{monthConverter(time.getMonth())}</p>
           </div>
           <h2 className="text-lg line-clamp-1">{props.email_subject}</h2>
           {/* Arrow Icon */}
--- src/components/EmailAccordion.js
+++ src/components/EmailAccordion.js
@@ -5,7 +5,10 @@ import Data from "../Lophils.json";
 function EmailAccordion() {
   const [open, setOpen] = useState(false);
   let data = Data.slice(0,10);
-  console.log(data);
+  data.sort((a, b) => {
+    return b.time_sent.localeCompare(a.time_sent);
+  })
+
   return(
     <div>
       <div className="emailBody box-border">
