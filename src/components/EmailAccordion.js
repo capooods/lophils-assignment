b748f54630fc3377461e413da8e79d68d8ef2e54
capooods <carlo.d.capio@gmail.com>
2022-10-20 14:40:53 +0800

    Added dataset

--- src/components/EmailAccordion.js
+++ src/components/EmailAccordion.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-
 import Accordion from './Accordion.js'
+import Data from "../Lophils.json";
 
 function EmailAccordion() {
   const [open, setOpen] = useState(false);
+  console.log(Data);
   return(
     <div>
       <div className="emailBody box-border">
---
+++ src/components/Tag.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Tag() {
+
+}
+
+export default Tag;
